5880265df568fd7600166cfc3dcb8f1272d6dba5
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-11-13 09:48:36 -0500

    Made query URL

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,2 +1,4 @@
 import { DomSelectors } from "./DomSelectors.js";
 DomSelectors.container.innerHTML = "<h1>JS Connected</h1>";
+pokedexnumber = Math.floor((Math.random() * 893) + 1);
+let queryurl = `https://pokeapi.co/api/v2/${pokedexnumber}`
