721cf9fe1bcba81eff737aa5498f267b3c828e61
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-11-13 17:24:45 -0500

    Made API Calling Function

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,9 +1,14 @@
 import { DomSelectors } from "./DomSelectors.js";
+import 'regenerator-runtime/runtime'
 DomSelectors.container.innerHTML = "<h1>JS Connected</h1>";
-let pokedexnumber = Math.floor((Math.random() * 893) + 1);
-let queryurl = `https://pokeapi.co/api/v2/${pokedexnumber}`;
+let pokedexnumber = Math.floor(Math.random() * 893 + 1);
+let queryurl = `https://pokeapi.co/api/v2/pokemon/${pokedexnumber}`;
+console.log(queryurl);
 
-async function searchthepokemon(){
-    return Pokemoninfo = Promise.resolve(queryurl);
-}
-searchthepokemon.then(console.log(Pokemoninfo));
+async function searchthepokemon() {
+    const response = await fetch(queryurl,['GET']);
+    const pokemon = await response.json();
+    return pokemon;
+} 
+
+
