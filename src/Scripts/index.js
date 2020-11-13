2becaf18465612af6e8279019bbf89f6cf44af00
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-11-13 18:06:31 -0500

    Removed testing code

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,14 +1,11 @@
 import { DomSelectors } from "./DomSelectors.js";
-import 'regenerator-runtime/runtime'
+import "regenerator-runtime/runtime";
 DomSelectors.container.innerHTML = "<h1>JS Connected</h1>";
 let pokedexnumber = Math.floor(Math.random() * 893 + 1);
 let queryurl = `https://pokeapi.co/api/v2/pokemon/${pokedexnumber}`;
-console.log(queryurl);
 
 async function searchthepokemon() {
-    const response = await fetch(queryurl,['GET']);
-    const pokemon = await response.json();
-    return pokemon;
-} 
-
-
+  const response = await fetch(queryurl, ["GET"]);
+  const pokemon = await response.json();
+  return pokemon;
+}
